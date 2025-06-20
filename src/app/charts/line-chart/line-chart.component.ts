faa6b360214dfbebbf9214c115f445cff2447c44
HUGO LACRUZ HARO <hugo@msm.local>
2025-06-20 10:14:06 +0200

    Data-Page: Added the line with multi Axis and mixed chart

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,50 +1,74 @@
 import {Component, Input} from '@angular/core';
-import {Chart, registerables} from 'chart.js';
+import {Chart, ChartData, ChartOptions, registerables} from 'chart.js';
 import {UserData} from '../../models/userData';
 import {BaseChartDirective} from 'ng2-charts';
 
 Chart.register(...registerables)
 
 @Component({
-    selector: 'app-line-chart',
-    imports: [
-        BaseChartDirective
-    ],
-    templateUrl: './line-chart.component.html',
-    styleUrl: './line-chart.component.scss'
+  selector: 'app-line-chart',
+  imports: [
+    BaseChartDirective
+  ],
+  templateUrl: './line-chart.component.html',
+  styleUrl: './line-chart.component.scss'
 })
 export class LineChartComponent {
 
-    @Input({required: true}) userData!: UserData[];
+  @Input({required: true}) userData!: UserData[];
 
-    loadData() {
-        return {
-            labels: this.getUserNames(),
-            datasets: [
-                {
-                    label: 'Tacos',
-                    data: this.getUserTacos(),
-                    backgroundColor: 'red'
-                },
-            ]
+  loadData(): ChartData<'line'> {
+    return {
+      labels: this.getUserNames(),
+      datasets: [
+        {
+          label: 'User Tacos',
+          data: this.getUserTacos(),
+          yAxisID: 'y',
+        },
+        {
+          label: 'User Time',
+          data: this.getUserTime(),
+          yAxisID: "y1",
+        },
+      ]
+    }
+  }
+
+  loadOptions(): ChartOptions<'line'> {
+    return {
+      maintainAspectRatio: true,
+      responsive: true,
+      scales: {
+        y: {
+          type: 'linear',
+          position: 'left',
+          title: {
+            display: true,
+            text: 'User Tacos'
+          }
+        }, y1: {
+          type: 'linear',
+          position: 'right',
+          title: {
+            display: true,
+            text: 'User time'
+          }
         }
+      }
     }
+  }
 
-    loadOptions() {
-        return {
-            maintainAspectRatio: true,
-            responsive: true
-        }
-    }
+  getUserTacos(): number[] {
+    return this.userData.map(item => item.tacos);
+  }
 
-    getUserTacos(): number[] {
-        return this.userData.map(item => item.tacos);
-    }
+  getUserNames(): string[] {
+    return this.userData.map(item => item.name);
+  }
 
-    getUserNames(): string[] {
-        return this.userData.map(item => item.name);
-    }
-
-
+  getUserTime(): number[] {
+    return this.userData.map(item => item.time);
+  }
 }
 
--- src/app/charts/mixed-chart/mixed-chart.component.ts
+++ src/app/charts/mixed-chart/mixed-chart.component.ts
@@ -1,11 +1,78 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {UserData} from '../../models/userData';
+import {BaseChartDirective} from 'ng2-charts';
+import {ChartData, ChartOptions} from 'chart.js';
 
 @Component({
   selector: 'app-mixed-chart',
-  imports: [],
+  imports: [
+    BaseChartDirective
+  ],
   templateUrl: './mixed-chart.component.html',
   styleUrl: './mixed-chart.component.scss'
 })
 export class MixedChartComponent {
+  @Input() userData1!: UserData[];
+  @Input() userData2!: UserData[];
 
+
+  loadData(): ChartData {
+    return {
+      labels: this.getUserNames1(),
+      datasets: [
+        {
+          type: 'bar',
+          label: 'User Tacos',
+          data: this.getUserTacos1(),
+          yAxisID: 'y',
+        },
+        {
+          type: 'line',
+          label: 'User Time',
+          data: this.getUserTime1(),
+          yAxisID: "y1",
+        },
+      ]
+    }
+  }
+
+  loadOptions(): ChartOptions {
+    return {
+      maintainAspectRatio: true,
+      responsive: true,
+      scales: {
+        y: {
+          type: 'linear',
+          position: 'left',
+          title: {
+            display: true,
+            text: 'User Tacos'
+          }
+        }, y1: {
+          type: 'linear',
+          position: 'right',
+          title: {
+            display: true,
+            text: 'User time'
+          }
+        }
+      }
+    }
+  }
+
+  getUserTacos1(): number[] {
+    return this.userData1.map(item => item.tacos);
+  }
+
+  getUserNames1(): string[] {
+    return this.userData1.map(item => item.name);
+  }
+
+  getUserTime1(): number[] {
+    return this.userData1.map(item => item.time);
+  }
+
+  getUserNames2(): string[] {
+    return this.userData2.map(item => item.name);
+  }
 }
